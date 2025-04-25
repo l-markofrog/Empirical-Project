054ee1e2b5b76faaef13795baf823d27d31fd07b
l-markofrog <l.markofrog@gmail.com>
2025-04-26 00:38:06 +0100

    Finished scrape fucntion. Reworked webpages scrape for links for listings using the function. Scraped for the number of pages

--- scrap.py
+++ scrap.py
@@ -2,7 +2,7 @@ from urllib import request, error
 import re
 
 starting_page = "https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on/"
-url_start = "https://www.rightmove.co.uk/"
+url_start = "https://www.rightmove.co.uk"
 
 def scrape(page, start, end):
     data_list = []
@@ -16,20 +16,23 @@ def scrape(page, start, end):
 
 #output = open("appartments.txt", "w")
 #output.write('Comic, Number, Title\n')
+appartment_links_list_ex = ['https://www.rightmove.co.uk//properties/160619321', 'https://www.rightmove.co.uk//properties/160687214', 'https://www.rightmove.co.uk//properties/160959065', 'https://www.rightmove.co.uk//properties/160617632', 'https://www.rightmove.co.uk//properties/159990269', 'https://www.rightmove.co.uk//properties/161155151', 'https://www.rightmove.co.uk//properties/159978746', 'https://www.rightmove.co.uk//properties/159478985', 'https://www.rightmove.co.uk//properties/160227788', 'https://www.rightmove.co.uk//properties/158884949', 'https://www.rightmove.co.uk//properties/159186707', 'https://www.rightmove.co.uk//properties/161155013', 'https://www.rightmove.co.uk//properties/160394129', 'https://www.rightmove.co.uk//properties/161154677', 'https://www.rightmove.co.uk//properties/161154926', 'https://www.rightmove.co.uk//properties/161154296', 'https://www.rightmove.co.uk//properties/161154434', 'https://www.rightmove.co.uk//properties/161154269', 'https://www.rightmove.co.uk//properties/161154266', 'https://www.rightmove.co.uk//properties/159704990', 'https://www.rightmove.co.uk//properties/157622354', 'https://www.rightmove.co.uk//properties/160587920', 'https://www.rightmove.co.uk//properties/160297301', 'https://www.rightmove.co.uk//properties/161153993', 'https://www.rightmove.co.uk//properties/161153996']
 
 appartment_links_list = []
-appartment_links_list_ex = ['https://www.rightmove.co.uk//properties/160619321', 'https://www.rightmove.co.uk//properties/160687214', 'https://www.rightmove.co.uk//properties/160959065', 'https://www.rightmove.co.uk//properties/160617632', 'https://www.rightmove.co.uk//properties/159990269', 'https://www.rightmove.co.uk//properties/161155151', 'https://www.rightmove.co.uk//properties/159978746', 'https://www.rightmove.co.uk//properties/159478985', 'https://www.rightmove.co.uk//properties/160227788', 'https://www.rightmove.co.uk//properties/158884949', 'https://www.rightmove.co.uk//properties/159186707', 'https://www.rightmove.co.uk//properties/161155013', 'https://www.rightmove.co.uk//properties/160394129', 'https://www.rightmove.co.uk//properties/161154677', 'https://www.rightmove.co.uk//properties/161154926', 'https://www.rightmove.co.uk//properties/161154296', 'https://www.rightmove.co.uk//properties/161154434', 'https://www.rightmove.co.uk//properties/161154269', 'https://www.rightmove.co.uk//properties/161154266', 'https://www.rightmove.co.uk//properties/159704990', 'https://www.rightmove.co.uk//properties/157622354', 'https://www.rightmove.co.uk//properties/160587920', 'https://www.rightmove.co.uk//properties/160297301', 'https://www.rightmove.co.uk//properties/161153993', 'https://www.rightmove.co.uk//properties/161153996']
 
 target = request.urlopen(starting_page)
 find_page = target.read()
 
-pre_link = find_page.decode('utf-8').split('<a data-testid="property-details-lozenge" href="')[1:]
 
-for data in pre_link:
-    link = data.split('#/?')[0]
-    appartment_links_list.append(url_start + link)
 
-print(len(appartment_links_list_ex))
+appartment_links_list = scrape(find_page, '<a data-testid="property-details-lozenge" href="', '#/?')
+appartment_links_list = [url_start + item for item in appartment_links_list]
+
+num_pages = int(scrape(find_page, 'of <!-- -->', '</span>'))
+
+print(appartment_links_list)
+print(len(appartment_links_list))
+print(num_pages)
 
 """
 for i in range(50):
