ef35d6fb7e1da08cdeb444fe83929d9db733e947
l-markofrog <l.markofrog@gmail.com>
2025-04-27 19:54:55 +0100

    Added several searches for 6 different London regions in order to distinguish between them and increase number of datapoints. Commited final csv file with appartments data.

--- scrape.py
+++ scrape.py
@@ -2,7 +2,20 @@ from urllib import request
 import csv 
 
 # Page found by hand which searches for appartments in London
-starting_page = "https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on/"
+
+all_regions = []
+E_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92825&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=East-London"
+all_regions.append(E_page)
+N_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92826&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=North-London"
+all_regions.append(N_page)
+SE_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92828&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=South-East-London"
+all_regions.append(SE_page)
+SW_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92829&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=South-West-London"
+all_regions.append(SW_page)
+NW_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92827&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=North-West-London"
+all_regions.append(NW_page)
+W_page = "https://www.rightmove.co.uk/property-to-rent/find.html?useLocationIdentifier=true&locationIdentifier=REGION%5E92830&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index=0&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=West-London"
+all_regions.append(W_page)
 # Start of the urls on the web-site
 url_start = "https://www.rightmove.co.uk"
 
@@ -17,10 +30,6 @@ def scrape(page, start, end):
         return data_list[0]
     else: return data_list
 
-# Search for the number of pages in the search result
-target = request.urlopen(starting_page)
-search_page = target.read()
-num_pages = int(scrape(search_page, 'of <!-- -->', '</span>'))
 
 # File of an output
 csvfile = open('appartments.csv', 'w', newline='')
@@ -28,65 +37,72 @@ csvfile = open('appartments.csv', 'w', newline='')
 spamwriter = csv.writer(csvfile, delimiter=',',
                         quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-spamwriter.writerow(['link', 'post_code', 'price_pcm', 'type', 'bedrooms', 'bathrooms'])
+spamwriter.writerow(['link', 'region', 'price_pcm', 'type', 'bedrooms', 'bathrooms'])
 
-
-# Scrape links for all appartments on all search pages. Link pattern is behind "index" and was found by hand
 data = []
 
-for i in range(num_pages):
-    print("page:",i+1)
-    try:
-        find_page = f"https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index={str(i*24)}&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=London-87490"
-        target = request.urlopen(find_page)
-        search_page = target.read()
-        print(find_page)
-        page_data = []
+for starting_page in all_regions:
+    # Search for the number of pages in the search result
+    target = request.urlopen(starting_page)
+    search_page = target.read()
+    num_pages = int(scrape(search_page, 'of <!-- -->', '</span>'))
 
-        appartment_links_list = scrape(search_page, '<a class="PropertyPrice_priceLink__b24b5" href="', '?')
-        appartment_links_list = [url_start + link for link in appartment_links_list]
-        print("appartment_links_list:", len(appartment_links_list))
-        page_data.append(appartment_links_list)
 
-        appartment_post_codes_list = scrape(search_page,'<address class="PropertyAddress_address__LYRPq" aria-label="Property address: ', '">')
-        appartment_post_codes_list = [post_code.split(', ')[-1] for post_code in appartment_post_codes_list]
-        print("appartment_post_codes_list:", len(appartment_post_codes_list))
-        page_data.append(appartment_post_codes_list)
-        
-        appartment_price_pcm_list = scrape(search_page, '<div class="PropertyPrice_price__VL65t">£', ' pcm</div>')
-        appartment_price_pcm_list = [int(price_pcm.replace(',','')) for price_pcm in appartment_price_pcm_list]
-        print("appartment_price_pcm_list:", len(appartment_price_pcm_list))
-        page_data.append(appartment_price_pcm_list)
-        
-        appartment_type_list = scrape(search_page,'<span class="PropertyInformation_propertyType__u8e76" aria-label="', '">')
-        print("appartment_type_list:", len(appartment_type_list))
-        page_data.append(appartment_type_list)
-        studios = [j for j, x in enumerate(appartment_type_list) if "Studio" in x]
+    # Scrape links for all appartments on all search pages
 
-        check_missing_list = scrape(search_page,'<div class="PropertyInformation_container__2wY0G" data-testid="property-information">', '</div></div></a>')
-        missing_bed = [j for j, x in enumerate(check_missing_list) if "PropertyInformation_bedContainer___rN7d" not in x]
-        missing_bath = [j for j, x in enumerate(check_missing_list) if "PropertyInformation_bathContainer__ut8VY" not in x]
-        
-        appartment_bedrooms_list = scrape(search_page, '<span class="PropertyInformation_bedroomsCount___2b5R" aria-label="',' in property">')
-        appartment_bedrooms_list = [int(beds) for beds in appartment_bedrooms_list]
-        for j in missing_bed: appartment_bedrooms_list.insert(j, '')
-        print("appartment_bedrooms_list:", len(appartment_bedrooms_list))
-        page_data.append(appartment_bedrooms_list)
 
-        appartment_bathrooms_list = scrape(search_page, '<span aria-label="',' in property">')
-        appartment_bathrooms_list = [int(baths) for baths in appartment_bathrooms_list]
-        for j in missing_bath: appartment_bathrooms_list.insert(j, '')
-        print("appartment_bathrooms_list:", len(appartment_bathrooms_list))
-        page_data.append(appartment_bathrooms_list)
-        
+    for i in range(num_pages):
+        print("page:",i+1)
+        try:
+            find_page = ("index="+str(i*24)).join(starting_page.split("index=0"))
+            target = request.urlopen(find_page)
+            search_page = target.read()
+            print(find_page)
+            page_data = []
 
-        pivot_data = [list(x) for x in zip(*page_data)]
+            appartment_links_list = scrape(search_page, '<a class="PropertyPrice_priceLink__b24b5" href="', '?')
+            appartment_links_list = [url_start + link for link in appartment_links_list]
+            print("appartment_links_list:", len(appartment_links_list))
+            page_data.append(appartment_links_list)
 
-        data = data + pivot_data
-        print("data on page:", len(pivot_data), "\n")
+            appartment_regions_list = scrape(starting_page.encode('utf-8'),'displayLocationIdentifier=', '-London')
+            appartment_regions_list = ["".join([region.replace("-"," ") for region in appartment_regions_list])] * len(appartment_links_list)
+            print("appartment_regions_list:", len(appartment_regions_list), appartment_regions_list[0])
+            page_data.append(appartment_regions_list)
+            
+            appartment_price_pcm_list = scrape(search_page, '<div class="PropertyPrice_price__VL65t">£', ' pcm</div>')
+            appartment_price_pcm_list = [int(price_pcm.replace(',','')) for price_pcm in appartment_price_pcm_list]
+            print("appartment_price_pcm_list:", len(appartment_price_pcm_list))
+            page_data.append(appartment_price_pcm_list)
+            
+            appartment_type_list = scrape(search_page,'<span class="PropertyInformation_propertyType__u8e76" aria-label="', '">')
+            print("appartment_type_list:", len(appartment_type_list))
+            page_data.append(appartment_type_list)
 
-    except Exception as e:
-        print(f"Error {e} on link: https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index={str(i*24)}&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=London-87490")
+            check_missing_list = scrape(search_page,'<div class="PropertyInformation_container__2wY0G" data-testid="property-information">', '</div></div></a>')
+            missing_bed = [j for j, x in enumerate(check_missing_list) if "PropertyInformation_bedContainer___rN7d" not in x]
+            missing_bath = [j for j, x in enumerate(check_missing_list) if "PropertyInformation_bathContainer__ut8VY" not in x]
+            
+            appartment_bedrooms_list = scrape(search_page, '<span class="PropertyInformation_bedroomsCount___2b5R" aria-label="',' in property">')
+            appartment_bedrooms_list = [int(beds) for beds in appartment_bedrooms_list]
+            for j in missing_bed: appartment_bedrooms_list.insert(j, '')
+            print("appartment_bedrooms_list:", len(appartment_bedrooms_list))
+            page_data.append(appartment_bedrooms_list)
+
+            appartment_bathrooms_list = scrape(search_page, '<span aria-label="',' in property">')
+            appartment_bathrooms_list = [int(baths) for baths in appartment_bathrooms_list]
+            for j in missing_bath: appartment_bathrooms_list.insert(j, '')
+            print("appartment_bathrooms_list:", len(appartment_bathrooms_list))
+            page_data.append(appartment_bathrooms_list)
+            
+
+            pivot_data = [list(x) for x in zip(*page_data)]
+
+            data = data + pivot_data
+            print("data on page:", len(pivot_data), "\n")
+
+        except Exception as e:
+            print(f"Error {e} on link: https://www.rightmove.co.uk/property-to-rent/find.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent&radius=0.0&propertyTypes=flat&_includeLetAgreed=on%2F&index={str(i*24)}&sortType=6&channel=RENT&transactionType=LETTING&displayLocationIdentifier=London-87490")
         
 for listing in data:
     spamwriter.writerow(listing)
